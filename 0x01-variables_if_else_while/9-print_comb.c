182fe5d09afaec49c96ba3b4b50d67741b4c35b7
dinahkabuo <dinahkabuo@gmail.com>
2023-03-18 01:47:40 -0700

    9printcomb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ *main - Entry point 
  *
  * Return: always 0 (Success)
  */
@@ -8,7 +8,7 @@ int main(void)
 {
 	int n;
 
-	for(n = 48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 		if (n != 57)
