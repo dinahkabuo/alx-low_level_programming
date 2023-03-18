5915eb780e4ccdc3723ab18e08acdaf4dd5fb32b
dinahkabuo <dinahkabuo@gmail.com>
2023-03-18 00:03:17 -0700

    1lastdigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ else if (ld == 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6band not 0\n", n, ld)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 }
 return (0);
 }
