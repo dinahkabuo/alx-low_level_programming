9ae537e12edb2c95df186d1df0840829d8189072
dinahkabuo <dinahkabuo@gmail.com>
2023-03-18 00:56:29 -0700

    5printnumbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*8
+ * main - Entry point
+ *
+ * Return: always 0 (Succes)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
