ecd8be539e6cd226445ddff98692ae146a587218
dinahkabuo <dinahkabuo@gmail.com>
2023-03-21 14:57:30 -0700

    6abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - computes absolut value of integer
+ *
+ * @n: integer to be evaluated
+ *
+ * Return: returns absolut value
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
