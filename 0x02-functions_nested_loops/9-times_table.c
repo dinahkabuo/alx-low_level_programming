e2a87402c49d10a434f186cdb4320fe84463f411
dinahkabuo <dinahkabuo@gmail.com>
2023-03-22 02:31:19 -0700

    9xtable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * times_table - prints te 9 times table
+ * starting with zero
+ *
+ * Return: always 0 (Success)
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			c = a * b;
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			else
+			{
+				if ((c / 10) == 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+			}
+		}
+	}
+}
