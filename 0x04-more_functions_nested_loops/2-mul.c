4c59cd07ceac615721c51b434d570988dd81fb1b
dinahkabuo <dinahkabuo@gmail.com>
2023-03-23 15:16:44 -0700

    4multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - multiplies two numbers
+ *
+ * @a: integer number
+ * @b: integer number
+ *
+ * Return: returns the multiplication result
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
