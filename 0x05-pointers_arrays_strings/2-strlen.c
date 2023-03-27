eaa1db183953cbad09a9c5f98a41d645cba59f19
dinahkabuo <dinahkabuo@gmail.com>
2023-03-27 14:42:30 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int 1;
+
+	1 = strlen(s);
+	return (1);
+}
