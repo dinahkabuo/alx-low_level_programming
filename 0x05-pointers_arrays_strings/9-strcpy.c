60f7e02e9f5261c20fc8726611600e82d129c01a
dinahkabuo <dinahkabuo@gmail.com>
2023-03-27 17:47:47 -0700

    9 string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strcpy - copies a string into anotehr
+ * @dest: string to be added to
+ * @src: string to be copied
+ * Return: nothing (void)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
