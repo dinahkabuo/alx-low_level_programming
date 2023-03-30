029a409e4c1fe13895e14052d68c2bedc7b795af
dinahkabuo <dinahkabuo@gmail.com>
2023-03-30 09:43:19 -0700

    3 compare string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strcmp - compares two strings
+ * @s1: string to be compared
+ * @s2: string to be compared
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
