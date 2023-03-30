ba77e0765295daaf8859589e83a249aac2fce0a2
dinahkabuo <dinahkabuo@gmail.com>
2023-03-30 07:45:26 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strcat - concatenates strings
+ * @dest: string to concatenate
+ * @src: string to be added
+ * Return: concatenated dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
