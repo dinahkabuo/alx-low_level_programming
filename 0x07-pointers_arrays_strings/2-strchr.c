eacac96e3d71142f6a0dc4d673caea183b0a091a
dinahkabuo <dinahkabuo@gmail.com>
2023-04-03 16:47:29 -0700

    locate character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer
+ * @c: char
+ * Return: char *
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
