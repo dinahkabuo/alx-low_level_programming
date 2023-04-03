f11297c3c601080828a5cedda68c00f91b104882
dinahkabuo <dinahkabuo@gmail.com>
2023-04-03 15:34:06 -0700

    memset 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills a block of memory with specific value
+ * @s: pointer to memory are
+ * @b: char for constant byte
+ * @n: number of bytes
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
