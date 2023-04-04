05dbde35c998d117e89dbe721fd8652e79803fb3
dinahkabuo <dinahkabuo@gmail.com>
2023-04-03 18:53:39 -0700

    locates substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strstr - function that locates a substring
+ * @haystack: string
+ * @needle: substring in string
+ * Return: char *
+ */
+char *_strstr(char *haystack, *needle)
+{
+	return (strstr(haystack, needle));
+}
